3bdbf9083385c490854aa852fe1433f4abea94b2
beranekleon <leon.beranek06@gmail.com>
2026-02-05 00:54:29 +0100

    initial commit
    setup initial mvvm project structure

---
+++ ModManager.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ModManager.App.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ModManager.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ModManager.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
