2a07ea29fb8b52d992cacfa1049069432a333d91
beranekleon <leon.beranek06@gmail.com>
2026-02-05 14:30:42 +0100

    add file interaction dialogue
    added a dialogue so you may select a png-file in the app and it then displays the selected file

--- ModManager.App/ViewModels/MainWindowViewModel.cs
+++ ModManager.App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,36 @@
-﻿namespace ModManager.App.ViewModels;
+﻿using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ModManager.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private Bitmap? _selectedImage;
+    public Bitmap? SelectedImage
+    {
+        get => _selectedImage;
+        set => SetProperty(ref _selectedImage, value);
+    }
+
+    public IAsyncRelayCommand OpenFileCommand { get; }
+
+    public event System.Func<Task<IStorageFile?>>? RequestFilePicker;
+
+    public MainWindowViewModel()
+    {
+        OpenFileCommand = new AsyncRelayCommand(async () =>
+        {
+            if (RequestFilePicker != null)
+            {
+                var file = await RequestFilePicker.Invoke();
+                if (file != null)
+                {
+                    await using var stream =  await file.OpenReadAsync();
+                    SelectedImage = new Bitmap(stream);
+                }
+            }
+        });
+    }
 }
--- ModManager.App/Views/MainWindow.axaml.cs
+++ ModManager.App/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using ModManager.App.ViewModels;
 
 namespace ModManager.App.Views;
 
@@ -7,5 +11,25 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+        if (DataContext is MainWindowViewModel vm)
+        {
+            vm.RequestFilePicker += async () =>
+            {
+                var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                {
+                    Title = "Select an Image",
+                    FileTypeFilter = new[] { FilePickerFileTypes.ImagePng },
+                    AllowMultiple = false
+                });
+
+                return files.FirstOrDefault();
+            };
+        }
     }
 }
